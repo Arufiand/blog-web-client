3e1ce0dcedf53bb4c1f93a7e4fce02827bba3429
arufiand <alfiandannyarmanta@gmail.com>
2025-11-01 20:19:13 +0700

    feat: Add PostList component and integrate it into the main application

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PostsCreate from "./PostsCreate";
+import PostList from "./PostList";
 
 const App = () => {
     return <div className="container">
         <h1>Posts Create</h1>
         <PostsCreate />
+
+        <br/>
+        <h1>Posts List</h1>
+         <PostList />
     </div>;
 }
 
